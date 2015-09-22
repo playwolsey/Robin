9c442ebb35a7abf4b789f219201ded81de4740ef
cp <chengpengcp9@163.com>
2015-09-22 12:18:22 +0800

    config compress

--- config/express.js
+++ config/express.js
@@ -13,20 +13,26 @@ let express = require('express');
 let path = require('path');
 let favicon = require('serve-favicon');
 let swig = require('swig');
+let compression = require('compression');
 
 
 /**
  * Expose
  */
 module.exports = (app) => {
-    // set favicon
+    // Compression middleware (should be placed before express.static)
+    app.use(compression({
+        threshold: 512
+    }));
+
+    // Set favicon
     app.use(favicon(`${__dirname}/../app/public/favicon.ico`));
 
     // Static files middleware
     app.use(express.static(path.join(__dirname, '../app/public')));
-    
 
-    // set views path, template engine and default layout
+
+    // Set views path, template engine and default layout
     app.engine('html', swig.renderFile);
     app.set('views', path.join(__dirname, '../app/views'));
     app.set('view engine', 'html');
