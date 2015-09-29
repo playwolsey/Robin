10dc4ca92e5d0bb68690c58a94a94386e61d9f86
cp <chengpengcp9@163.com>
2015-09-29 11:12:45 +0800

    browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,40 @@ let concat = require('gulp-concat');
 let nodemon = require('gulp-nodemon');
 let browserSync = require('browser-sync').create();
 
-gulp.task('scripts', () => {
-    console.log('scripts');
+gulp.task('run', (cb) => {
+    let started = false;
+
+    nodemon({
+        execMap: {
+          js: 'node'
+        },
+        script: 'app.js'
+    })
+    .on('start', () => {
+        if (!started) {
+			cb();
+            started = true; 
+        } 
+    })
+    .on('restart', () => {
+        setTimeout(() => {
+            browserSync.reload({
+                stream: false
+            });
+        }, 500);
+    });
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('browser-sync', ['run'], () => {
+	browserSync.init(null, {
+		proxy: 'http://10.240.129.0:1999',
+        files: [
+            'app/public/**/*.*',
+            'app/components/**/*.*'
+        ],
+        browser: ['google chrome'],
+        port: 7000,
+	});
+});
+
+gulp.task('default', ['browser-sync']);
