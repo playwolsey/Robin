c28b185c9e7b0e4e6773f79c194ec5abadcd09f1
cp <chengpengcp9@163.com>
2015-09-30 17:49:50 +0800

    css watch

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,39 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
+const less = require('gulp-less');
+const cssmin = require('gulp-cssmin');
+const autoprefixer = require('gulp-autoprefixer');
+const plumber = require('gulp-plumber');
 const nodemon = require('gulp-nodemon');
 const browserSync = require('browser-sync').create();
 
-gulp.task('run', (cb) => {
+
+gulp.task('less', () => {
+    return gulp.src(['app/client/less/**/*.less', '!app/client/less/common/**/*.less', '!app/client/less/**/{reset,test}.less'])
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(autoprefixer())
+        .pipe(cssmin())
+        .pipe(gulp.dest('public/assets/css'))
+});
+
+gulp.task('watch', () => {
+    gulp.watch('app/client/less/**/*.less', ['less']);
+    //gulp.watch('public/javascripts/**/*.js', ['build']);
+    //gulp.watch('public/css/**/*.less', ['build']);
+    //gulp.watch('components/**/*.jsx', ['build']);
+    //gulp.watch('components/**/*.less', ['build']);
+});
+
+gulp.task('run', ['watch'], (cb) => {
     let started = false;
 
     nodemon({
-        execMap: {
-          js: 'node'
-        },
-        script: 'app.js',
+        //execMap: {
+        //  js: 'node'
+        //},
+        //script: 'app.js',
         exec: 'npm start'
     })
     .on('start', () => {
