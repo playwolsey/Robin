1d8a2539d72df50c7746a6c01bba5b40f5d2f935
cp <chengpengcp9@163.com>
2015-09-30 17:48:36 +0800

    wrapper init

---
+++ app/components/common/FormGroup.react.js
@@ -0,0 +1,33 @@
+/**
+ * @desc @jsx FormGroup React.DOM 
+ * @author awwwesssooooome <chengpengcp9@gmail.com>
+ * @date 2015-09-30
+ */
+
+'use strict';
+
+import React from 'react/addons';
+import classNames from 'classnames';
+
+const FormGroup = React.createClass({
+    propTypes: {
+        validation: React.PropTypes.string,
+        hasFeedback: React.PropTypes.bool
+    },
+
+    render() {
+        let classSet = {};
+
+        classSet.form-group = true;
+        this.props.validation && (classSet['form-' + this.props.validation] = true);
+        classSet.form-feedback = this.props.hasFeedback;
+
+        return (
+            <div className={classNames(classSet, this.props.className)}>
+                {this.props.children}
+            </div>
+        )
+    }
+});
+
+export default FormGroup;
