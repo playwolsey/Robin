d492aed65ce5364b862647899da6c57b3733a761
cp <chengpengcp9@163.com>
2015-10-15 10:24:50 +0800

    use express route just for api

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+/**
+ * @desc app server main file
+ * @author awwwesssooooome <chengpengcp9@gmail.com>
+ * @date 2015-09-21
+ */
+
 'use strict';
 
 /**
@@ -8,10 +14,12 @@ import http from 'http';
 
 import conf_express from './config/express';
 import conf_routes from './config/routes';
+import conf_api from './config/api';
 
 const app = express();
 
 conf_express(app);
+conf_api(app);
 conf_routes(app);
 
 app.set('port', process.env.PORT || 1999);
