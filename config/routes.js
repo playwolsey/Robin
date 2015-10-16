11e0ae64d4e7f1c298bc2cb50fbcbc65d88e6283
cp <chengpengcp9@163.com>
2015-10-16 11:12:48 +0800

    add 404 routes && opt

--- config/routes.js
+++ config/routes.js
@@ -10,22 +10,22 @@
  * Module dependencies
  */
 import React from 'react';
-//import {Router, Route} from 'react-router'; // not work, but api said ok
-import Router from 'react-router';
-import {Route} from 'react-router';
+import Router, {Route} from 'react-router';
 import swig from 'swig'; 
 import App from '../app/components/App.react';
 import Index from '../app/components/Index.react';
+import NoMatch from '../app/components/NoMatch.react';
 
 import pkg from '../package.json';
 
-export default ((app) => {
-    let routes = (
-        <Route handler={App}>
-            <Route path='/' handler={Index} />
-        </Route>
-    );
+export const routes = (
+    <Route handler={App}>
+        <Route path='/' handler={Index} />
+        <Route path="*" handler={NoMatch}/>
+    </Route>
+);
 
+export default ((app) => {
     app.use((req, res) => {
         Router.run(routes, req.path, (Handler) => {
             let html = React.renderToString(React.createElement(Handler));
